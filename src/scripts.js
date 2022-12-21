9f7996d5b95a4319f65b63e905658194552932a8
joaohrn <joaohrn0@gmail.com>
2022-12-21 18:58:26 -0300

    Finish the functionality and add css

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,20 @@
-function add(a, b) {
-  return a + b;
+function add(a = 0, b = 0) {
+  return +a + +b;
 }
-function subtract(a, b) {
-  return a - b;
+function subtract(a = 0, b = 0) {
+  return +a - +b;
 }
-function multiply(a, b) {
-  return a * b;
+function multiply(a = 0, b = 0) {
+  return +a * +b;
 }
-function divide(a, b) {
-  return a / b;
+function divide(a = 0, b = 1) {
+  if (+b === 0) {
+    return "please, just don't.";
+  } else {
+    return Math.round((+a / +b) * 1000000) / 1000000;
+  }
 }
-function operate(a, operator, b) {
+function operate(a, operator = "+", b = 0) {
   if (operator === "+") {
     return add(a, b);
   } else if (operator === "-") {
@@ -25,6 +29,8 @@ let displayText = "";
 const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
+const result = document.querySelector(".result");
+const clear = document.querySelector(".clear");
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
     displayText += e.target.dataset.number;
@@ -36,4 +42,20 @@ operators.forEach((operator) => {
     displayText += ` ${e.target.dataset.operator} `;
     display.textContent = displayText;
   });
+});
+result.addEventListener("click", () => {
+  operationArray = displayText.split(" ");
+  while (operationArray.length != 1) {
+    let newNumber = operate(
+      operationArray[0],
+      operationArray[1],
+      operationArray[2]
+    );
+    operationArray.splice(0, 3, newNumber);
+  }
+  display.textContent = operationArray[0];
+});
+clear.addEventListener("click", () => {
+  displayText = "";
+  display.textContent = "";
 });
